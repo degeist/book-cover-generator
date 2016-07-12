cf3004cb396c6ad9ab11e5c201616a62b1fcccf8
Erasmus Hagen <erasmus@degeist.com>
2016-07-12 09:29:47 +0200

    Remove font

---
+++ js/dropzone.js
@@ -0,0 +1,11 @@
+// http://www.dropzonejs.com/#configuration-options
+Dropzone.options.backgroundImageUpload = {
+  paramName: "file", // The name that will be used to transfer the file
+  maxFilesize: 2, // MB
+  accept: function(file, done) {
+    if (file.name == "justinbieber.jpg") {
+      done("Naha, you don't.");
+    }
+    else { done(); }
+  }
+};
