66364446733326a89fe52788ec12898432a38f45
André Avorio <a@avor.io>
2016-07-17 19:14:31 -0300

    Use PROJECT_ROOT instead of BASE_DIR for templates

--- bcg/settings.py
+++ bcg/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'bcg.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(PROJECT_ROOT, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
