04d1d595d93a8aeff3a0282315975be2cf9251af
André Avorio <a@avor.io>
2016-07-17 18:17:49 -0300

    Change structure of STATIC_ROOT

--- bcg/settings.py
+++ bcg/settings.py
@@ -121,7 +121,7 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
