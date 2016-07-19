0a86724965e2995dc479a8d4b48817bed8a56418
André Avorio <a@avor.io>
2016-07-19 10:24:43 -0300

    Remove SQLite

--- bcg/settings.py
+++ bcg/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'bcg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
