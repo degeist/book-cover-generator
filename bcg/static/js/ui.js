f9d5c7cd8015edeb3dc2eab5721857982bd8b63e
Erasmus Hagen <erasmus@degeist.com>
2016-07-20 22:00:21 +0200

    Fix #19

--- bcg/static/js/ui.js
+++ bcg/static/js/ui.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   // When selecting pre-made bg in modal, the artboard bg should change
   $('.select-background').click(function() {
-    var chosenImg = $(this).find('img').attr('src');
-    var chosenImgParent = chosenImg.replace('/thumbnails', '');
+    var chosenImg = $(this).find('img').attr('data-large-img');
+    $('#artboard').css("background-image", "url(" + chosenImg +")");
     $('#backgroundUploadModal').modal('hide');
   })
 
