2210fdcb1d64e9b1b0c37b6d438fa043aeffcdc1
André Avorio <a@avor.io>
2016-07-20 22:01:05 -0300

    Create Artboard model

--- artboard/models.py
+++ artboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+"""
+This is the main model in the project. It holds a reference to a
+cloudinary-stored image (background) and contains some additional
+data used in the construction of the final artboard image.
+"""
+
+class Artboard(models.Model):
+    text = models.CharField(max_length=240, blank=True)
+    image = CloudinaryField('image')
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_changed = models.DateTimeField(auto_now=True)
