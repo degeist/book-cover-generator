8daba64bb0ea436defef041ea489034a4fe37cba
André Avorio <a@avor.io>
2016-07-20 22:02:24 -0300

    Add default/local database setting

--- bcg/settings.py
+++ bcg/settings.py
@@ -62,7 +62,7 @@ WSGI_APPLICATION = 'bcg.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
+        'NAME': 'danandphil',
         'USER': '',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
