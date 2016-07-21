e9b3b322521a982ddbd4273bf5351b36f5c41562
André Avorio <a@avor.io>
2016-07-20 22:02:37 -0300

    Enable artboard Django app

--- bcg/settings.py
+++ bcg/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'whitenoise.runserver_nostatic', # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
     'django.contrib.staticfiles',
+    'artboard',
 ]
 
 MIDDLEWARE_CLASSES = [
