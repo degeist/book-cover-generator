7d1e4144753fd901e5f039215a61f7bc91766bb4
Erasmus Hagen <erasmus@degeist.com>
2016-07-21 21:38:56 +0200

    Integrate Cloudinary upload in frontend (#27)
    * Fix #20 (#26)
    
    * Forgot DEBUG false
    
    * 11 integrate cloudinary (#28)
    
    * Changing the artboard BG with uploaded img (WIP)
    
    * Cloudinary background upload basic integration

--- bcg/static/js/dropzone.js
+++ bcg/static/js/dropzone.js
@@ -1,24 +1,59 @@
 /*jslint unparam: true */
 /*global window, $ */
+// $(function () {
+//     'use strict';
+//     // Change this to the location of your server-side upload handler:
+//     var url = 'server/php/';
+//     $('#fileupload').fileupload({
+//         url: url,
+//         dataType: 'json',
+//         done: function (e, data) {
+//             $.each(data.result.files, function (index, file) {
+//                 $('<p/>').text(file.name).appendTo('#files');
+//             });
+//         },
+//         progressall: function (e, data) {
+//             var progress = parseInt(data.loaded / data.total * 100, 10);
+//             $('#progress .progress-bar').css(
+//                 'width',
+//                 progress + '%'
+//             );
+//         }
+//     }).prop('disabled', !$.support.fileInput)
+//         .parent().addClass($.support.fileInput ? undefined : 'disabled');
+// });
+
 $(function () {
-    'use strict';
-    // Change this to the location of your server-side upload handler:
-    var url = 'server/php/';
-    $('#fileupload').fileupload({
-        url: url,
-        dataType: 'json',
-        done: function (e, data) {
-            $.each(data.result.files, function (index, file) {
-                $('<p/>').text(file.name).appendTo('#files');
-            });
-        },
-        progressall: function (e, data) {
-            var progress = parseInt(data.loaded / data.total * 100, 10);
-            $('#progress .progress-bar').css(
-                'width',
-                progress + '%'
-            );
-        }
-    }).prop('disabled', !$.support.fileInput)
-        .parent().addClass($.support.fileInput ? undefined : 'disabled');
+  $('#direct_upload input[type="file"]')
+  .cloudinary_fileupload({
+    dropZone: '#direct_upload',
+    start: function () {
+      $('.status_value').text('Starting direct upload...');
+    },
+    progress: function () {
+      $('.status_value').text('Uploading...');
+    },
+  })
+  .on('cloudinaryprogress', function (e, data) {
+    $('.progress').show();
+    $('.progress').css('width',
+      Math.round((data.loaded * 100.0) / data.total) + '%');
+  })
+  .on('cloudinarydone', function (e, data) {
+      // $('.status_value').text('Updating backend...');
+      $.post(this.form.action, $(this.form).serialize()).always(function (result, status, jqxhr) {
+        $('.status_value').text(result.errors ? JSON.stringify(result.errors) : status);
+      });
+
+      // TODO separate as own function
+
+      var background = $('#artboard');
+      var uploadedBgImg = $.cloudinary.url(data.result.public_id, {
+        format: data.result.format, width: 500, height: 660, crop: "fill"
+      });
+      $(background).css("background-image", "url(" + uploadedBgImg +")");
+
+      $('#backgroundUploadModal').modal('hide');
+
+  });
 });
--- bcg/static/js/ui.js
+++ bcg/static/js/ui.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
+  // Setting base vars
+  var currentCutout = $('img#artboardCutout').attr('src');
+
+  // Open the background upload modal
+  $('#backgroundUpload').click(function() {
+    $('#backgroundUploadModal').modal('show');
+  });
+
+  // Open the cutouts modal & set the current modal
+  $('#artboardCutout').click(function() {
+    $('#cutoutsSelectorModal').modal('show');
+
+    $('.modal--cutouts-selector__option').each(function() {
+        if ($(this).attr("data-large-img") == currentCutout) {
+          $(this).addClass("modal--cutouts-selector__option--selected");
+        }
+      });
+    });
+
   // When selecting pre-made bg in modal, the artboard bg should change
   $('.select-background').click(function() {
     // Parse large image URL from the data-attr
@@ -7,14 +26,38 @@ $(document).ready(function() {
     $('#backgroundUploadModal').modal('hide');
   })
 
+  // Selecting another cutout
   $('.modal--cutouts-selector__option').click(function() {
-    // Parse large image URL from the data-attr
-    var chosenCutout = $(this).attr('data-large-img');
-    $('img#artboardCutout').attr('src', chosenCutout);
-    $('#cutoutsSelectorModal').modal('hide');
-  })
+    // Parse large image URL from the data-attr and replace the img on artboard
+    var clickedCutout = $(this).attr('data-large-img');
+
+    if (clickedCutout != currentCutout) {
+      $('img#artboardCutout').attr('src', clickedCutout);
+      $('.modal--cutouts-selector__option--selected').removeClass('modal--cutouts-selector__option--selected');
+      // Close modal
+      $('#cutoutsSelectorModal').modal('hide');
+      currentCutout = clickedCutout;
+    }
+  });
 
   // Inline text edit
   $('#userCoverTextEditable').inlineEdit('click');
+
+  // Hide helper text onclick
+  $('.artboard__actionable-item').click(function() {
+    $(this).children('.artboard__helper-text').hide('fast');
+  });
+
+  // Output variables
+
+  $('.finish').click(function() {
+    var bg = $('#artboard').css("background-image").replace('url(','').replace(')','');
+    var cutout = $('img#artboardCutout').attr('src');
+    var text = $('p#userCoverTextEditable').text();
+
+    alert("BackgroundURL: " + bg + "\nCutout img: " + cutout + "\nEdited text: " + text);
+
+  });
+
 
 });
